a0a48e0f40db755b05165e83d6f770b1ef1dfea6
slynobody <55579266+slynobody@users.noreply.github.com>
2024-05-26 21:14:29 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ sudo pacman-key --populate archlinux
 sudo pacman-key --populate holo
 sudo pacman -Syu
 sudo pacman -S gstreamer-vaapi gst-plugin-pipewire gst-plugins-bad-libs gst-plugins-good gst-plugins-ugly mpv python-websockets qt5-websockets --overwrite '*'
+tar -xvzf binaries/arch/sd/com/github/catsout/wallpaperEngineKde/libWallpaperEngineKde.tar.gz
+rm binaries/arch/sd/com/github/catsout/wallpaperEngineKde/libWallpaperEngineKde.tar.gz
 cp -r kde/new/plasma/wallpapers/com.github.casout.wallpaperEngineKde /home/deck/.local/share/plasma/wallpapers
 sudo cp -r binaries/arch/sd/com /usr/lib/qt/qml/com/
 #for testing purposes
