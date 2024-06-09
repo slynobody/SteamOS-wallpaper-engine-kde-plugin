a527bea3e863d91661298e1cce17c5f653dbbbef
slynobody <55579266+slynobody@users.noreply.github.com>
2024-06-09 15:30:09 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # wallpaper-engine alternative >> https://github.com/varietywalls/variety?tab=readme-ov-file
 # installer for arch (steam-deck), main lib-location is /usr/lib/qt/qml/com/github/catsout/wallpaperEngineKde/
+passwd
 sudo steamos-readonly disable
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
@@ -9,6 +10,6 @@ sudo pacman -Syu
 sudo pacman -S gstreamer-vaapi gst-plugin-pipewire gst-plugins-bad-libs gst-plugins-good gst-plugins-ugly mpv python-websockets qt5-websockets --overwrite '*'
 tar -xvzf binaries/arch/sd/com/github/catsout/wallpaperEngineKde/libWallpaperEngineKde.tar.gz -C binaries/arch/sd/com/github/catsout/wallpaperEngineKde/
 rm binaries/arch/sd/com/github/catsout/wallpaperEngineKde/libWallpaperEngineKde.tar.gz 
-sudo cp -r kde/new/plasma/wallpapers/com.github.casout.wallpaperEngineKde /home/deck/.local/share/plasma/wallpapers
+cp -r kde/new/plasma/wallpapers/com.github.casout.wallpaperEngineKde /home/deck/.local/share/plasma/wallpapers
 sudo cp -r binaries/arch/sd/com /usr/lib/qt/qml/
 sudo steamos-readonly enable
