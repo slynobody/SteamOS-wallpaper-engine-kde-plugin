cee73c6eca0dd18a887c6b26c19b0230e98e1e8e
slynobody <55579266+slynobody@users.noreply.github.com>
2024-10-31 17:58:28 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo pacman-key --populate archlinux
 sudo pacman-key --populate holo
 sudo pacman -Syu
 sudo pacman -S gstreamer-vaapi gst-plugin-pipewire gst-plugins-bad-libs gst-plugins-good gst-plugins-ugly mpv python-websockets qt5-websockets --overwrite '*'
-plasmapkg2 -r ~/.local/share/plasma/wallpapers/com.github.casout.wallpaperEngineKde
+plasmapkg2 -r ~/.local/share/plasma/wallpapers/com.github.casout.wallpaperEngineKde && qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file:///media/sda2/Background/SpaceWall/Escape_Function.jpg")}'
 tar -xvzf binaries/arch/sd/com/github/catsout/wallpaperEngineKde/libWallpaperEngineKde.tar.gz -C binaries/arch/sd/com/github/catsout/wallpaperEngineKde/
 rm binaries/arch/sd/com/github/catsout/wallpaperEngineKde/libWallpaperEngineKde.tar.gz 
 mkdir  ~/.local/share/plasma/
